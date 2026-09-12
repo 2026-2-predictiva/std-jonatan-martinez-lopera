146cd0e61bacf7bc864dbb4c21a06be95c81f387
Jonatan Martinez Lopera <jonmartinezlo@unal.edu.co>
2026-09-12 10:39:27 -0500

    Hola mundo

--- PRE_01_hola_mundo/src/pregunta_01.py
+++ PRE_01_hola_mundo/src/pregunta_01.py
@@ -8,10 +8,9 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 def pregunta_01():
     """
     Retorne el string "Hola mundo cruel!".
-
     """
 
-    return
+    return "Hola mundo cruel!"
 
 
 if __name__ == "__main__":
--- PRE_01_hola_mundo/src/pregunta_02.py
+++ PRE_01_hola_mundo/src/pregunta_02.py
@@ -8,11 +8,9 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 def pregunta_02():
     """
     Retorne el string "Hello cruel world!".
-
-
     """
 
-    return
+    return "Hello cruel world!"
 
 
 if __name__ == "__main__":
